90b612f406ad0c2a53095b5fd19c17327e569763
TaianRmos <87705384+TaianRmos@users.noreply.github.com>
2023-10-23 20:55:41 +0200

    Update write_vbs_file.py to add loops

--- scripts/write_vbs_file.py
+++ scripts/write_vbs_file.py
@@ -1,5 +1,28 @@
-def write_vbs_file(project_name, vbs_title, vbs_content):
-    content = f"""result = MsgBox("{vbs_content}", vbOKCancel, "{vbs_title}")
+def write_vbs_file(project_name, vbs_title, vbs_content, want_loop):
+    # Creates the content of the file depending on if the user wants a loop or not
+    if want_loop:
+        content = f"""result = MsgBox("{vbs_content}", vbOKCancel, "{vbs_title}")
+
+If result = vbOK Then
+    ' User clicked OK
+    Set objShell = CreateObject("WScript.Shell")
+    objShell.Run "{project_name}.exe", 1, False
+
+    Set objShell = CreateObject("WScript.Shell")
+    objShell.Run "wscript.exe {project_name}.vbs", 1, False
+
+Else
+    ' User clicked Cancel or closed the message box
+    Set objShell = CreateObject("WScript.Shell")
+    objShell.Run "{project_name}.exe", 1, False
+
+    Set objShell = CreateObject("WScript.Shell")
+    objShell.Run "wscript.exe {project_name}.vbs", 1, False
+
+End If"""
+    
+    else:
+        content = f"""result = MsgBox("{vbs_content}", vbOKCancel, "{vbs_title}")
 
 If result = vbOK Then
     ' User clicked OK
@@ -11,6 +34,7 @@ Else
     Set objShell = CreateObject("WScript.Shell")
     objShell.Run "{project_name}.exe", 1, True
 End If"""
+
     
     with open(f"{project_name}/{project_name}.vbs", 'w') as vbs_file:
         vbs_file.write(content)
